ab7c567cfade92b10369b9dfb78fa06b4e8098b3
TravisSLee <travisslee223@gmail.com>
2021-03-03 11:20:39 -0500

    commit tree 877f59fbc86c93904b59226c204f520721831bb1

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your functions here
+function map (arr, func) {
+    let newArr = [];
+
+    for (let i = 0; i < arr.length; i++)
+    {
+        newArr.push(func(arr[i]));
+    }
+
+    return newArr;
+}
+
+function reduce (arr, func, start) {
+    let smt;
+
+    if(start) {
+        smt = start;
+        for (let i = 0; i < arr.length; i++) {
+            smt = func(arr[i], smt);
+        }
+    }
+
+    else {
+        smt = arr[0];
+        for (let i = 1; i < arr.length; i++) {
+            smt = func(arr[i], smt);
+        }
+    }
+    return smt;
+}
